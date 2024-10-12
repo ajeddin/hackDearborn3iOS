5945742624802e9baf3e3a59ca8f8bb91a9a19c2
Abdulaziz Jamaleddin <abduljeddin@gmail.com>
2024-10-12 19:54:53 -0400

    Aj

---
+++ hd3/Views/ContentView.swift
@@ -0,0 +1,82 @@
+//
+//  ContentView.swift
+//  hd3
+//
+//  Created by Abdulaziz Jamaleddin on 10/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var text: String = ""
+    @State private var date = Date()
+    var body: some View {
+        GeometryReader{ geoProx in
+            NavigationView{
+                VStack{
+                    VStack{
+                        HStack
+                        {
+                            NavigationLink{
+                                calendarView()
+                            } label: {
+                                Image(systemName: "calendar")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width:geoProx.size.width/20,height: geoProx.size.height/20 )
+                                
+                            }
+                            
+                            //                        Image(systemName: "calendar")
+                            Spacer()
+                            NavigationLink{
+                                chatView()
+                            } label: {
+                                Image(systemName: "person.fill")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width:geoProx.size.width/20,height: geoProx.size.height/20 )
+                                
+                            }
+                            
+                        }
+                        Divider().bold().frame(height: 10)
+                        HStack{
+                            Text("Today").bold().font(.title)
+                            Spacer()
+                            
+                        }
+                    }.padding(.top,50)                    .padding([.bottom,.leading,.trailing]).ignoresSafeArea()
+                    Spacer()
+                    HStack{
+                        TextField("Search", text: $text)
+                            .padding()
+                            .background(Color(.systemGray6))
+                            .cornerRadius(25)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 25)
+                                    .stroke(Color.gray, lineWidth: 1)
+                            )
+                        Spacer()
+                        
+                        Image(systemName: "arrow.right")
+                    }.padding()
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                }/*.padding().ignoresSafeArea()*/
+            }
+
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
