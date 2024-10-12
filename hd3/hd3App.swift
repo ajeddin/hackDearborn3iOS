fcf7ca192a8e8a0cdc17a27b8eab39a6d8bc71c6
Abdulaziz Jamaleddin <abduljeddin@gmail.com>
2024-10-12 16:43:22 -0400

    Initial

---
+++ hd3/hd3App.swift
@@ -0,0 +1,17 @@
+//
+//  hd3App.swift
+//  hd3
+//
+//  Created by Abdulaziz Jamaleddin on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct hd3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hd3Tests/hd3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  hd3Tests.swift
+//  hd3Tests
+//
+//  Created by Abdulaziz Jamaleddin on 10/12/24.
+//
+
+import Testing
+@testable import hd3
+
+struct hd3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
